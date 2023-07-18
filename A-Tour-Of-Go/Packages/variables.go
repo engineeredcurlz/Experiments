e68be2747240afd3d0584d93d184bec9b6d0eb0a
Diamond <32712461+engineeredcurlz@users.noreply.github.com>
2023-07-18 18:56:19 +0000

    variables.go

---
+++ A-Tour-Of-Go/Packages/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
+
+
+
+// var declares a list of variables aka function argument lists
+// the type is usually addressed last
+// var can either be at the package or function level
