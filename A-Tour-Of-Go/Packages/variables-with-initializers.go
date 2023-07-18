1d7aaf86a586017b4e7c740317c9913d6ff55422
Diamond <32712461+engineeredcurlz@users.noreply.github.com>
2023-07-18 18:56:12 +0000

    variables-with-intializers.go

---
+++ A-Tour-Of-Go/Packages/variables-with-initializers.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true, false. "no!"
+	fmt.Println(i, j , c, python java)
+}
+
+// var can include initializers, one per variable
+// if an initializer is present, the type can be ignored
+// the variable will take the type of the initializer instead
