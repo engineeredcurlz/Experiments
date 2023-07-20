84e2a96182baf5adaf6262c88973f084e52b7e52
Diamond <32712461+engineeredcurlz@users.noreply.github.com>
2023-07-20 18:30:58 +0000

    Added Counter

--- Prometheus-Up-&-Running/Chapter 3/Prometheus.py
+++ Prometheus-Up-&-Running/Chapter 3/Prometheus.py
@@ -1,8 +1,15 @@
 import http.server
 from prometheus_client import start_http_server
+from prometheus_client import Counter # 1.
+
+# 1. Tracks the number of times "Hello World" has been requested
+## Counter is defined as "hello_worlds_total, with the helper string "Hellow Worlds requested" that will show on the /metrics page to show what that metric actually means
+REQUESTS = Counter('hello_worlds_total',
+        'Hello Worlds requested.')
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        REQUESTS.inc() # 1. Call the metric object. inc method increments the counters value by 1
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
@@ -11,3 +18,6 @@ if __name__ == "__main__":
     start_http_server(8000) # Starts up an HTTP server on port 8000 to serve metrics to Prometheus
     server = http.server.HTTPServer(('localhost', 8001), MyHandler)
     server.serve_forever()
+
+
+#1 Counters can be used to track errors and unexpected situations. Knowing how errors are trending over time is good for debugging. Knowing the popular features or code paths of an application helps to optimize and allocate dev efforts.
